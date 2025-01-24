f1e14ddb2b291a5442b29001627262e73342c1bb
hayato-n8810 <s276185@wakayama-u.ac.jp>
2025-01-24 17:05:07 +0900

    対応するfastコードの検索と出力

--- server/hash.py
+++ server/hash.py
@@ -11,11 +11,21 @@ def get_features(s):
 
 # データ
 SLOW_FILE = 'codes_slow.json'
+FAST_FILE = 'codes_fast.json'
+
+# 遅いコードデータ
 try:
     with open(SLOW_FILE, "r", encoding='utf-8') as f:
         codes_slow = json.load(f)
 except UnicodeDecodeError:
         print(f"Skipping file due to encoding issue: {SLOW_FILE}")
+# 速いコードデータ
+try:
+    with open(FAST_FILE, "r", encoding='utf-8') as f:
+        codes_fast = json.load(f)
+except UnicodeDecodeError:
+        print(f"Skipping file due to encoding issue: {FAST_FILE}")
+
 
 # Simhash オブジェクトを作成
 hashes = {key: Simhash(get_features(value)) for key, value in codes_slow.items()}
@@ -28,8 +38,9 @@ input_hash = Simhash(get_features(input_string))
 threshold = 0.8
 bit_length = 64
 
-# 近似重複キーを取得
+# 類似した文字列のキー
 similar_keys = []
+# 近似重複キーを取得
 for key, hash_obj in hashes.items():
     distance = input_hash.distance(hash_obj)
     similarity = 1 - (distance / bit_length)
@@ -39,5 +50,15 @@ for key, hash_obj in hashes.items():
 # 出力
 if similar_keys:
     print(f"類似した文字列のキー: {similar_keys}")
+    for slow_key in similar_keys:
+        # 正規表現で数字を抽出
+        numbers = re.findall(r'\d+', slow_key)
+        fast_key = "fast" + str(numbers[0])
+
+        # マッチしたslowコードに対応するfastコードを検索
+        print(fast_key)
+        fast_code = codes_fast[fast_key].strip().replace('\n', ' ')  # 改行文字を半角スペースに置換
+        print(fast_code)
+        print()
 else:
     print("類似した文字列が見つかりませんでした。")
