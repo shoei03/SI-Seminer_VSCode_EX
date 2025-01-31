2b12d765bf64920b864e90c0f554b8413a833487
hayato-n8810 <s276185@wakayama-u.ac.jp>
2025-01-31 16:20:14 +0900

    renderのURLへのapiアクセスに変更

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.callAPI = callAPI;
 async function callAPI(precode) {
-    const res = await fetch(`http://127.0.0.1:8000/${precode}`);
+    const res = await fetch(`https://mayslowtofastapi.onrender.com/${precode}`);
     const data = await res.json();
     return data;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export async function callAPI(precode: string) {
-    const res = await fetch(`http://127.0.0.1:8000/${precode}`);
+    const res = await fetch(`https://mayslowtofastapi.onrender.com/${precode}`);
     const data = await res.json();
     return data;
 }
